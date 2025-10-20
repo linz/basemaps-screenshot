f34db3b5b71c9556542054214018e65758d91d49
Wentao Kuang <wkuang@linz.govt.nz>
2025-10-21 12:37:27 +1300

    feat: Add screenshot tests for topographic-v2 maps. BM-1356 (#432)
    ### Motivation
    
    Update all the topographic tests to point to topgraphic-v2 data now.
    And only kept four old topographic tests as we will stop supporting them
    soon.
    
    ### Modifications
    
    - Refactored all `topographic` basemap tests to reference
    `topographic-v2` instead.
    - Copied a select few of the existing `topographic` tests to the end of
    the test list.
    - Grouped similar tests under heading, such as:
    
      ```
      /**
       * topographic-v2: north island
       */
      ```
    
    - Added detail to non-descriptive names, such as:
    
      | Before | After |
      | - | - |
      | `topographic-v2-3857-z8` | `topographic-v2-3857-north-island-z8` |
    
    
    - Referenced test locations by constants, such as:
    
      | Before | After |
      | - | - |
    | `location: { lat: -41.8899962, lng: 174.0492437, z: 5 },` | `location:
    { ...Locs.Mainland, z: 5 },` |
    
    ### Verification
    
    ---------
    
    Co-authored-by: Tawera Manaena <TManaena@linz.govt.nz>

--- src/tiles.ts
+++ src/tiles.ts
@@ -21,165 +21,253 @@ export interface TestTile {
   hillshade?: string;
 }
 
+const Locs = {
+  Mainland: { lat: -41.8899962, lng: 174.0492437 },
+  NorthIsland: { lat: -39.2169833, lng: 176.4774344 },
+  SouthIsland: { lat: -45.0735206, lng: 169.1674805 },
+  ChathamIslands: { lat: -43.9090948, lng: -176.540438 },
+
+  WellingtonUrban: { lat: -41.2890657, lng: 174.7769262 },
+  ChristchurchNorthUrban: { lat: -43.4567506, lng: 172.6109426 },
+
+  Akaroa: { lat: -43.8063936, lng: 172.9679876 },
+  AucklandAirport: { lat: -37.000845, lng: 174.8064383 },
+  CanterburyRural: { lat: -43.4040409, lng: 172.5393086 },
+  MountCookVillage: { lat: -43.717227, lng: 170.0844837 },
+  MountTaranaki: { lat: -39.3031717, lng: 174.0585208 },
+  Ngauranga: { lat: -41.2454458, lng: 174.8101136 },
+  OtakiSouth: { lat: -40.7727954, lng: 175.1504838 },
+} as const;
+
 export const DefaultTestTiles: TestTile[] = [
+  /**
+   * health: mainland
+   */
   {
-    name: 'health-3857-z5',
+    name: 'health-3857-mainland-z5',
     tileMatrix: TileMatrixIdentifier.Google,
-    location: { lat: -41.8899962, lng: 174.0492437, z: 5 },
+    location: { ...Locs.Mainland, z: 5 },
     tileSet: 'health',
   },
   {
-    name: 'health-2193-z3',
+    name: 'health-2193-mainland-z3',
     tileMatrix: TileMatrixIdentifier.Nztm2000Quad,
-    location: { lat: -41.8899962, lng: 174.0492437, z: 3 },
+    location: { ...Locs.Mainland, z: 3 },
     tileSet: 'health',
   },
+  /**
+   * aerial: specific locations
+   */
   {
     name: 'aerial-3857-wellington-urban-z16',
     tileMatrix: TileMatrixIdentifier.Google,
-    location: { lat: -41.2890657, lng: 174.7769262, z: 16 },
+    location: { ...Locs.WellingtonUrban, z: 16 },
     tileSet: 'aerial',
   },
   {
     name: 'aerial-3857-canterbury-rural-z12',
     tileMatrix: TileMatrixIdentifier.Google,
-    location: { lat: -43.4040409, lng: 172.5393086, z: 12 },
+    location: { ...Locs.CanterburyRural, z: 12 },
     tileSet: 'aerial',
   },
+  /**
+   * topographic-v2: north island
+   */
   {
-    name: 'topographic-3857-z8',
+    name: 'topographic-v2-3857-north-island-z8',
     tileMatrix: TileMatrixIdentifier.Google,
-    location: { lat: -39.2169833, lng: 176.4774344, z: 8 },
+    location: { ...Locs.NorthIsland, z: 8 },
+    tileSet: 'topographic-v2',
+    style: 'topographic-v2',
+  },
+  /**
+   * topolite-v2: north island
+   */
+  {
+    name: 'topolite-v2-3857-north-island-z8',
+    tileMatrix: TileMatrixIdentifier.Google,
+    location: { ...Locs.NorthIsland, z: 8 },
+    tileSet: 'topographic-v2',
+    style: 'topolite-v2',
+  },
+  /**
+   * topographic-v2: specific locations
+   */
+  {
+    name: 'topographic-v2-3857-mainland-z14',
+    tileMatrix: TileMatrixIdentifier.Google,
+    location: { ...Locs.Mainland, z: 14 },
+    tileSet: 'topographic-v2',
+    style: 'topographic-v2',
+  },
+  {
+    name: 'topographic-v2-3857-ngauranga-z15',
+    tileMatrix: TileMatrixIdentifier.Google,
+    location: { ...Locs.Ngauranga, z: 15 },
+    tileSet: 'topographic-v2',
+    style: 'topographic-v2',
+  },
+  {
+    name: 'topographic-v2-3857-auckland-airport-z13',
+    tileMatrix: TileMatrixIdentifier.Google,
+    location: { ...Locs.AucklandAirport, z: 13 },
+    tileSet: 'topographic-v2',
+    style: 'topographic-v2',
+  },
+  {
+    name: 'topographic-v2-3857-otaki-south-z13',
+    tileMatrix: TileMatrixIdentifier.Google,
+    location: { ...Locs.OtakiSouth, z: 13 },
+    tileSet: 'topographic-v2',
+    style: 'topographic-v2',
+  },
+  {
+    name: 'topographic-v2-3857-christchurch-north-urban-z17',
+    tileMatrix: TileMatrixIdentifier.Google,
+    location: { ...Locs.ChristchurchNorthUrban, z: 17 },
+    tileSet: 'topographic-v2',
+    style: 'topographic-v2',
+  },
+  {
+    name: 'topographic-v2-3857-mount-cook-village-z12',
+    tileMatrix: TileMatrixIdentifier.Google,
+    location: { ...Locs.MountCookVillage, z: 12 },
+    tileSet: 'topographic-v2',
+    style: 'topographic-v2',
+  },
+  /**
+   * topolite-v2: specific locations
+   */
+  {
+    name: 'topolite-v2-3857-ngauranga-z15',
+    tileMatrix: TileMatrixIdentifier.Google,
+    location: { ...Locs.Ngauranga, z: 15 },
+    tileSet: 'topographic-v2',
+    style: 'topolite-v2',
+  },
+  {
+    name: 'topolite-v2-3857-akaroa-z17',
+    tileMatrix: TileMatrixIdentifier.Google,
+    location: { ...Locs.Akaroa, z: 17 },
+    tileSet: 'topographic-v2',
+    style: 'topolite-v2',
+  },
+  /**
+   * aerial: terrain + hillshade, hillshade, terrain
+   */
+  {
+    name: 'aerial-3857-south-island-terrain-hillshade-z6',
+    tileMatrix: TileMatrixIdentifier.Google,
+    location: { ...Locs.SouthIsland, z: 6 },
+    tileSet: 'aerial',
+    style: 'aerial',
+    terrain: 'LINZ-Terrain',
+    hillshade: 'LINZ-Terrain',
+  },
+  {
+    name: 'aerial-3857-mainland-hillshade-z5',
+    tileMatrix: TileMatrixIdentifier.Google,
+    location: { ...Locs.Mainland, z: 5 },
+    tileSet: 'aerial',
+    style: 'aerial',
+    hillshade: 'LINZ-Terrain',
+  },
+  {
+    name: 'aerial-3857-mount-taranaki-terrain-z14',
+    tileMatrix: TileMatrixIdentifier.Google,
+    location: { ...Locs.MountTaranaki, z: 14, p: 43 },
+    tileSet: 'aerial',
+    style: 'aerial',
+    terrain: 'LINZ-Terrain',
+  },
+  /**
+   * topographic-v2: terrain + hillshade, hillshade
+   */
+  {
+    name: 'topographic-v2-3857-mainland-terrain-hillshade-z5',
+    tileMatrix: TileMatrixIdentifier.Google,
+    location: { ...Locs.Mainland, z: 5 },
+    tileSet: 'topographic-v2',
+    style: 'topographic-v2',
+    terrain: 'LINZ-Terrain',
+    hillshade: 'LINZ-Terrain',
+  },
+  /**
+   * topolite-v2: hillshade
+   */
+  {
+    name: 'topolite-v2-3857-mainland-hillshade-z4',
+    tileMatrix: TileMatrixIdentifier.Google,
+    location: { ...Locs.Mainland, z: 4 },
+    tileSet: 'topographic-v2',
+    style: 'topolite-v2',
+    hillshade: 'LINZ-Terrain',
+  },
+  /**
+   * topo-raster: specific locations
+   */
+  {
+    // verify the absence of black edge artifacts around the imagery's perimeter
+    name: 'topo-gridless-maps-2193-mainland-250k-z4',
+    tileMatrix: TileMatrixIdentifier.Nztm2000Quad,
+    location: { ...Locs.Mainland, z: 4 },
+    tileSet: 'topo-raster',
+    style: 'topo-raster',
+  },
+  {
+    // verify the location and re-projection of the chatham islands imagery
+    name: 'topo-gridless-maps-3857-chatham-islands-250k-z10',
+    tileMatrix: TileMatrixIdentifier.Google,
+    location: { ...Locs.ChathamIslands, z: 10 },
+    tileSet: 'topo-raster',
+    style: 'topo-raster',
+  },
+  {
+    // verify the tileset displays the 50k imagery
+    name: 'topo-gridless-maps-3857-wellington-urban-50k-z12',
+    tileMatrix: TileMatrixIdentifier.Google,
+    location: { ...Locs.WellingtonUrban, z: 12 },
+    tileSet: 'topo-raster',
+    style: 'topo-raster',
+  },
+
+  // TODO: We will keep support for these tilesets for 90 days, please remove the tests after deprecating
+
+  /**
+   * topographic: north island
+   */
+  {
+    name: 'topographic-3857-north-island-z8',
+    tileMatrix: TileMatrixIdentifier.Google,
+    location: { ...Locs.NorthIsland, z: 8 },
     tileSet: 'topographic',
     style: 'topographic',
   },
+  /**
+   * topolite: north island
+   */
   {
-    name: 'topolite-3857-z8',
+    name: 'topolite-3857-north-island-z8',
     tileMatrix: TileMatrixIdentifier.Google,
-    location: { lat: -39.2169833, lng: 176.4774344, z: 8 },
+    location: { ...Locs.NorthIsland, z: 8 },
     tileSet: 'topographic',
     style: 'topolite',
   },
-  {
-    name: 'topographic-3857-z14',
-    tileMatrix: TileMatrixIdentifier.Google,
-    location: { lat: -41.8899962, lng: 174.0492437, z: 14 },
-    tileSet: 'topographic',
-    style: 'topographic',
-  },
-  {
-    name: 'topographic-3857-ngauranga-z15',
-    tileMatrix: TileMatrixIdentifier.Google,
-    location: { lat: -41.2454458, lng: 174.8101136, z: 15 },
-    tileSet: 'topographic',
-    style: 'topographic',
-  },
-  {
-    name: 'topographic-3857-auckland-airport-z13',
-    tileMatrix: TileMatrixIdentifier.Google,
-    location: { lat: -37.000845, lng: 174.8064383, z: 13 },
-    tileSet: 'topographic',
-    style: 'topographic',
-  },
-  {
-    name: 'topographic-3857-otaki-south-z13',
-    tileMatrix: TileMatrixIdentifier.Google,
-    location: { lat: -40.7727954, lng: 175.1504838, z: 13 },
-    tileSet: 'topographic',
-    style: 'topographic',
-  },
-  {
-    name: 'topographic-3857-christchurch-north-urban-z17',
-    tileMatrix: TileMatrixIdentifier.Google,
-    location: { lat: -43.4567506, lng: 172.6109426, z: 17 },
-    tileSet: 'topographic',
-    style: 'topographic',
-  },
+  /**
+   * topographic: specific locations
+   */
   {
     name: 'topographic-3857-mount-cook-village-z12',
     tileMatrix: TileMatrixIdentifier.Google,
-    location: { lat: -43.717227, lng: 170.0844837, z: 12 },
+    location: { ...Locs.MountCookVillage, z: 12 },
     tileSet: 'topographic',
     style: 'topographic',
   },
   {
     name: 'topolite-3857-ngauranga-z15',
     tileMatrix: TileMatrixIdentifier.Google,
-    location: { lat: -41.2454458, lng: 174.8101136, z: 15 },
+    location: { ...Locs.Ngauranga, z: 15 },
     tileSet: 'topographic',
     style: 'topolite',
-  },
-  {
-    name: 'topolite-3857-z17',
-    tileMatrix: TileMatrixIdentifier.Google,
-    location: { lat: -43.8063936, lng: 172.9679876, z: 17 },
-    tileSet: 'topographic',
-    style: 'topolite',
-  },
-  {
-    name: 'aerial-3857-terrain-hillshade-z6',
-    tileMatrix: TileMatrixIdentifier.Google,
-    location: { lat: -45.0735206, lng: 169.1674805, z: 6 },
-    tileSet: 'aerial',
-    style: 'aerial',
-    terrain: 'LINZ-Terrain',
-    hillshade: 'LINZ-Terrain',
-  },
-  {
-    name: 'aerial-3857-terrain-hillshade-z5',
-    tileMatrix: TileMatrixIdentifier.Google,
-    location: { lat: -40.7681948, lng: 172.4544741, z: 5 },
-    tileSet: 'aerial',
-    style: 'aerial',
-    hillshade: 'LINZ-Terrain',
-  },
-  {
-    name: 'aerial-3857-mount-taranaki-terrain-hillshade-z14',
-    tileMatrix: TileMatrixIdentifier.Google,
-    location: { lat: -39.3031717, lng: 174.0585208, z: 14, p: 43 },
-    tileSet: 'aerial',
-    style: 'aerial',
-    terrain: 'LINZ-Terrain',
-  },
-  {
-    name: 'topographic-3857-terrain-hillshade-z12',
-    tileMatrix: TileMatrixIdentifier.Google,
-    location: { lat: -42.4146988, lng: 174.1766669, z: 5 },
-    tileSet: 'topographic',
-    style: 'topographic',
-    terrain: 'LINZ-Terrain',
-    hillshade: 'LINZ-Terrain',
-  },
-  {
-    name: 'topolite-3857-hillshade-z4',
-    tileMatrix: TileMatrixIdentifier.Google,
-    location: { lat: -41.6092133, lng: 173.0748696, z: 4 },
-    tileSet: 'topographic',
-    style: 'topolite',
-    hillshade: 'LINZ-Terrain',
-  },
-  {
-    // verify the absence of black edge artifacts around the imagery's perimeter
-    name: 'nz-topo-gridless-maps-2193-nz-250k-z4',
-    tileMatrix: TileMatrixIdentifier.Nztm2000Quad,
-    location: { lat: -40.7899231, lng: 172.2977984, z: 4 },
-    tileSet: 'topo-raster',
-    style: 'topo-raster',
-  },
-  {
-    // verify the location and re-projection of the chatham islands imagery
-    name: 'nz-topo-gridless-maps-3857-chatham-islands-250k-z10',
-    tileMatrix: TileMatrixIdentifier.Google,
-    location: { lat: -43.9090948, lng: -176.540438, z: 10 },
-    tileSet: 'topo-raster',
-    style: 'topo-raster',
-  },
-  {
-    // verify the tileset displays the 50k imagery
-    name: 'nz-topo-gridless-maps-3857-wellington-50k-z12',
-    tileMatrix: TileMatrixIdentifier.Google,
-    location: { lat: -41.299807, lng: 174.8041651, z: 12 },
-    tileSet: 'topo-raster',
-    style: 'topo-raster',
   },
 ];
